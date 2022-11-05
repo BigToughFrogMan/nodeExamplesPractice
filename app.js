baa369dc9b2fc8805da9465d26f3b5a8f0198050
Robert Duddy <rduddy13@gmail.com>
2022-11-05 00:34:44 -0400

    callbacks and error handling in node

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const rect = require('./rectangle')
 function solveRect(l, w) {
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`)
 
-    if (l <= 0 || w <= 0) {
-        console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`)
-    } else {
-        console.log(`Area of rectangle: ${rect.area(l,w)}`)
-        console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`)
-    }
+//next lines only necessary if using the export in shorthand to define methods for perimeter and area. when we made the entire export have a function value we included the error check within it in the module
+    // if (l <= 0 || w <= 0) {
+    //     console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`)
+
+    rect(l, w, (err, rectangle) => {
+        if (err) {
+            console.log('ERROR:', err.message)
+        } else {
+            console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`)
+            console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`)
+        }
+    })
+    console.log('This statement is logged after the call to rect()')
 }
 
 solveRect(2, 4)
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,19 @@
-exports.perimeter = (x, y) => 2 * (x + y)
-exports.area = (x, y) => x * y
+//exports.perimeter = (x, y) => 2 * (x + y)
+//exports.area = (x, y) => x * y
+//exports.function is a shorthand for module.exports
+// you can do that for defining a method on exports
+//instead we will use the full module.exports to assign a full function as a value to the entire export instead of as a method to the export
+
+module.exports = (x, y, callback) => {
+    if (x <= 0 || y <= 0) {
+        callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`))
+    } else {
+        setTimeout(() =>
+            callback(null, {
+                perimeter: () => 2 * (x + y),
+                area: () => x * y
+            }),
+            2000
+        )
+    }
+}
